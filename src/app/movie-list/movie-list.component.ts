b0d908647444079ae478802c72b2167cf4a5c8ae
FrankKamala <frankkamala@gmail.com>
2020-01-22 20:06:53 +0300

    add ApiResponse

--- src/app/movie-list/movie-list.component.ts
+++ src/app/movie-list/movie-list.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import {HttpClient} from '@angular/common/http'; //import http
+import {Movies} from '../movies';
+
 @Component({
   selector: 'app-movie-list',
   templateUrl: './movie-list.component.html',
   styleUrls: ['./movie-list.component.css']
 })
 export class MovieListComponent implements OnInit {
+  movies:Movies;
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
   ngOnInit() {
+    interface ApiResponse {
+      title: string;
+      releaseYear: number;
+      writer: string;
+      duration:number;
+      summary:string;
+      review:string
+     }
+      this.http.get<ApiResponse>('https://adwera-movie-api.herokuapp.com').subscribe(data => this.movies = data);
   }
 
 }
