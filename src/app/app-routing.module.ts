ae350a2aefe5d04360a27d805888bb4d06064b9a
FrankKamala <frankkamala@gmail.com>
2020-01-22 13:02:49 +0300

    add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MovieListComponent } from './movie-list/movie-list.component';
+import { HorrorListComponent } from './horror-list/horror-list.component';
+import { ComedyListComponent } from './comedy-list/comedy-list.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'movieList', component: MovieListComponent},
+  { path: 'horrorList', component: HorrorListComponent},
+  { path: 'comedyList', component: ComedyListComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
