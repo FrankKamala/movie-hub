896c87774687d56171e853bc84fcf5be4815ae59
FrankKamala <frankkamala@gmail.com>
2020-01-22 20:01:42 +0300

    add movie class

---
+++ src/app/movies.ts
@@ -0,0 +1,3 @@
+export class Movies {
+    constructor( public title: string, public releaseYear:number, public writer:string, public duration:number, public summary: string,public review:string){}
+}
