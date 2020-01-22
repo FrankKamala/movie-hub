6d9ba726f50429ea8a65e293de03b1762361c638
FrankKamala <frankkamala@gmail.com>
2020-01-22 12:44:56 +0300

    added comedy component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { MovieListComponent } from './movie-list/movie-list.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HorrorListComponent } from './horror-list/horror-list.component';
+import { ComedyListComponent } from './comedy-list/comedy-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MovieListComponent,
     NavbarComponent,
-    HorrorListComponent
+    HorrorListComponent,
+    ComedyListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/comedy-list/comedy-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-comedy-list',
+  templateUrl: './comedy-list.component.html',
+  styleUrls: ['./comedy-list.component.css']
+})
+export class ComedyListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
