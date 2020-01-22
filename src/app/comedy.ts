7f47039c79174c7e7415863a62308264e9126787
FrankKamala <frankkamala@gmail.com>
2020-01-22 15:53:04 +0300

    fix writer

--- src/app/comedy.ts
+++ src/app/comedy.ts
@@ -1,4 +1,4 @@
 export class Comedy {
-  constructor( public title: string, public releaseyear:number, public writter:string, public duration:number; public summary: string){
+  constructor( public title: string, public releaseyear:number, public writer:string, public duration:number; public summary: string){
            }
 }
